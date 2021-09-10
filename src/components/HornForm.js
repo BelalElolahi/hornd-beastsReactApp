f6d635eb8330ec7f253afb400502a5d01b2902cb
BelalElolahi <belalelolahi003@gmail.com>
2021-09-10 14:44:32 -0700

    finished hornBest

--- src/components/HornForm.js
+++ src/components/HornForm.js
@@ -19,10 +19,10 @@ export class HornForm extends Component {
                     <Form.Group as={Col} controlId="formGridState" >
                         <Form.Select  onChange ={(event) => {this.props.SelecttionHornFilter(event);}}>
                             <option value="0">select...</option> 
-                            <option value="1"> One horn</option> 
-                            <option value="2"> two horns</option> 
-                            <option value="3"> three horns</option> 
-                            <option value="100"> onehandred horns</option> 
+                            <option value="1"> 1 horn</option> 
+                            <option value="2"> 2 horns</option> 
+                            <option value="3"> 3 horns</option> 
+                            <option value="100"> 100 horns</option> 
                         </Form.Select>
                     </Form.Group>
                     
